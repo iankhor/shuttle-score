4459a4c8867cbd54446fdd3586a854081fd0fad5
iankhor <iankhorbc.thecoder@gmail.com>
2017-04-17 12:27:31 +1000

    added court scoring mechanism

--- src/components/court/Court.js
+++ src/components/court/Court.js
@@ -1,83 +1,109 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
+import { Badge } from 'reactstrap'
 
 class Court extends Component {
+        constructor(props){
+          super(props)
+
+          this.state = {
+            scoreLeftPlayer: 0,
+            scoreRightPlayer: 0,
+          }
+
+        }
+
+        add = (scoredSide) => {
+            scoredSide === "left" ?
+              this.setState( { scoreLeftPlayer: this.state.scoreLeftPlayer + 1 } ) :
+              this.setState( { scoreRightPlayer: this.state.scoreRightPlayer + 1 } )
+        }
+
     render(){
         return(
-          <div className="court">
 
-            {/* Top Side line */}
-            <div className="sideline"> 
-              {/* Left Side Back Court*/}
-              <div className="courtline-topside courtline-left sideline-back "></div>
-              {/* Left Side Mid Court*/}
-              <div className="courtline-topside sideline-mid"></div>
-              {/* Left Side Front Court*/}
-              <div className="courtline-topside sideline-front"></div>
+          <div>
+          <Row>
+            <Col>
+               <Badge className="text-score" pill color="info" >{this.state.scoreLeftPlayer}</Badge>
+            </Col>
+            <Col>
+               <Badge className="text-score" pill color="info" >{this.state.scoreRightPlayer}</Badge>
+            </Col>
+          </Row>
+          
+            <Row className="court">
 
-              {/* Right Side Front Court*/}
-              <div className="courtline-topside sideline-front"></div>
-              {/* Right Side Mid Court*/}
-              <div className="courtline-topside sideline-mid"></div>
-              {/* Right Side Back Court*/}
-              <div className="courtline-topside courtline-right sideline-back"></div>
-            </div>
+              {/* Top Side line */}
+              <div className="sideline"> 
+                {/* Left Side Back Court*/}
+                <div onClick={ this.add.bind(null,"left") } className="courtline-topside courtline-left sideline-back "></div>
+                {/* Left Side Mid Court*/}
+                <div onClick={ this.add.bind(null,"left") } className="courtline-topside sideline-mid"></div>
+                {/* Left Side Front Court*/}
+                <div onClick={ this.add.bind(null,"left") } className="courtline-topside sideline-front"></div>
 
-            {/* Top Mid court*/}
-            <div className="midcourt"> 
-              {/* Left Mid Back Court*/}
-              <div className="courtline-leftside midcourt-back"></div>
-              {/* Left Mid Court Left*/}
-              <div className="courtline midcourt-mid"></div>
-              {/* Left Mid Front Court*/}
-              <div className="courtline countline-midright midcourt-front"></div>
+                {/* Right Side Front Court*/}
+                <div onClick={ this.add } className="courtline-topside sideline-front"></div>
+                {/* Right Side Mid Court*/}
+                <div onClick={ this.add } className="courtline-topside sideline-mid"></div>
+                {/* Right Side Back Court*/}
+                <div onClick={ this.add } className="courtline-topside courtline-right sideline-back"></div>
+              </div>
 
-              {/* Right Mid Front Court*/}
-              <div className="courtline countline-midleft midcourt-front"></div>
-              {/* Right Mid Court Left*/}
-              <div className="courtline midcourt-mid"></div>
-              {/* Right Mid Back Court*/}
-              <div className="courtline-rightside midcourt-back"></div>
-            </div>
+              {/* Top Mid court*/}
+              <div className="midcourt"> 
+                {/* Left Mid Back Court*/}
+                <div onClick={ this.add.bind(null,"left") } className="courtline-leftside midcourt-back"></div>
+                {/* Left Mid Court Left*/}
+                <div onClick={ this.add.bind(null,"left") } className="courtline midcourt-mid"></div>
+                {/* Left Mid Front Court*/}
+                <div onClick={ this.add.bind(null,"left") } className="courtline countline-midright midcourt-front"></div>
 
-            {/* Bottom Mid court*/}
-            <div className="midcourt"> 
-              {/* Left Mid Back Court*/}
-              <div className="courtline-leftside midcourt-back"></div>
-              {/* Left Mid Court Left*/}
-              <div className="courtline midcourt-mid"></div>
-              {/* Left Mid Front Court*/}
-              <div className="courtline countline-midright midcourt-front"></div>
+                {/* Right Mid Front Court*/}
+                <div onClick={ this.add } className="courtline countline-midleft midcourt-front"></div>
+                {/* Right Mid Court Left*/}
+                <div onClick={ this.add } className="courtline midcourt-mid"></div>
+                {/* Right Mid Back Court*/}
+                <div onClick={ this.add } className="courtline-rightside midcourt-back"></div>
+              </div>
 
-              {/* Right Mid Front Court*/}
-              <div className="courtline countline-midleft midcourt-front"></div>
-              {/* Right Mid Court Left*/}
-              <div className="courtline midcourt-mid"></div>
-              {/* Right Mid Back Court*/}
-              <div className="courtline-rightside midcourt-back"></div>
-            </div>
+              {/* Bottom Mid court*/}
+              <div className="midcourt"> 
+                {/* Left Mid Back Court*/}
+                <div onClick={ this.add.bind(null,"left") } className="courtline-leftside midcourt-back"></div>
+                {/* Left Mid Court Left*/}
+                <div onClick={ this.add.bind(null,"left") } className="courtline midcourt-mid"></div>
+                {/* Left Mid Front Court*/}
+                <div onClick={ this.add.bind(null,"left") } className="courtline countline-midright midcourt-front"></div>
 
-             {/* Bottom Side line */}
-            <div className="sideline"> 
-              {/* Left Side Back Court*/}
-              <div className="courtline-bottomside courtline-left sideline-back "></div>
-              {/* Left Side Mid Court*/}
-              <div className="courtline-bottomside sideline-mid"></div>
-              {/* Left Side Front Court*/}
-              <div className="courtline-bottomside sideline-front"></div>
+                {/* Right Mid Front Court*/}
+                <div onClick={ this.add } className="courtline countline-midleft midcourt-front"></div>
+                {/* Right Mid Court Left*/}
+                <div onClick={ this.add } className="courtline midcourt-mid"></div>
+                {/* Right Mid Back Court*/}
+                <div onClick={ this.add } className="courtline-rightside midcourt-back"></div>
+              </div>
 
-              {/* Right Side Front Court*/}
-              <div className="courtline-bottomside sideline-front"></div>
-              {/* Right Side Mid Court*/}
-              <div className="courtline-bottomside sideline-mid"></div>
-              {/* Right Side Back Court*/}
-              <div className="courtline-bottomside courtline-right sideline-back"></div>
-            </div>
+              {/* Bottom Side line */}
+              <div className="sideline"> 
+                {/* Left Side Back Court*/}
+                <div onClick={ this.add.bind(null,"left") } className="courtline-bottomside courtline-left sideline-back "></div>
+                {/* Left Side Mid Court*/}
+                <div onClick={ this.add.bind(null,"left") } className="courtline-bottomside sideline-mid"></div>
+                {/* Left Side Front Court*/}
+                <div onClick={ this.add.bind(null,"left") } className="courtline-bottomside sideline-front"></div>
 
-
-            
-
+                {/* Right Side Front Court*/}
+                <div onClick={ this.add } className="courtline-bottomside sideline-front"></div>
+                {/* Right Side Mid Court*/}
+                <div onClick={ this.add } className="courtline-bottomside sideline-mid"></div>
+                {/* Right Side Back Court*/}
+                <div onClick={ this.add } className="courtline-bottomside courtline-right sideline-back"></div>
+              </div>
+            </Row>
           </div>
+          
         )
 
     }
